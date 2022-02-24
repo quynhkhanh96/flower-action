89553348b29a52be9db82d61dab009396c6b465a
quynhkhanh96 <quynhkhanh.hust59@gmail.com>
2022-02-24 15:51:18 +0700

    minor change in fedavg_server

--- federated_learning/server/fedavg_server.py
+++ federated_learning/server/fedavg_server.py
@@ -27,7 +27,7 @@ class FedAvgStrategy(flwr.server.strategy.FedAvg):
             from models import MLP as Fed_Model
         elif self.model_name == 'ResNet':
             from models import ResNet as Fed_Model 
-        elif cfgs.model == 'MobileNetV3':
+        elif self.model == 'MobileNetV3':
             from models import MobileNetV3 as Fed_Model
         net = Fed_Model(self.num_classes)
         net.set_weights(weights)

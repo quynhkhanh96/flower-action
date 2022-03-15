b112a5a72571f675c0a86b55c1c7779140e9c65c
quynhkhanh96 <oddball9796@gmail.com>
2022-03-15 15:52:24 +0700

    add iid partition for hmdb51

--- datasets/hmdb51_mmaction2.py
+++ datasets/hmdb51_mmaction2.py
@@ -1,7 +1,43 @@
+import os 
+import pickle 
+import shutil 
+import sys 
 from mmaction.datasets import build_dataset
 
-def hmdb51_partition(n_clients):
-    pass    
+def hmdb51_partition(n_clients, preprocessed_dir, fold):
+    """
+    Args:
+        preprocessed_dir (str): path to directory containing HMDB51's 
+                                `rawframes`, `videos`, split files (.txt)
+    """
+    os.makedirs(preprocessed_dir + f'/partition', exist_ok=True)
+    all_actions = {action: [] 
+                    for action in os.listdir(preprocessed_dir + '/rawframes')}
+    with open(preprocessed_dir + f'/hmdb51_train_split_{fold}_rawframes.txt', 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        action, _ = line.split('/')
+        all_actions[action].append(line)
+    for action in all_actions:
+        all_actions[action] = [all_actions[action][i::n_clients] for i in range(n_clients)]
+
+    for client_id in range(n_clients):
+        client_files = []
+        for action in all_actions:
+            client_files.extend(all_actions[action][client_id])
+        metadata_path = '{}/partition/hmdb51_train_split_{}_client_{}_rawframes.txt'.format(
+            preprocessed_dir, fold, client_id
+        )
+        with open(metadata_path, 'r') as f:
+            for client_file in client_files:
+                f.write(client_file + '\n')
+
+    shutil.copy(preprocessed_dir + f'/hmdb51_val_split_{fold}_rawframes.txt',
+                preprocessed_dir + f'/partition/hmdb51_val_split_{fold}_rawframes.txt')
+    
+    with open(preprocessed_dir + '/partition/clients.pkl', 'wb') as handle:
+        pickle.dump(all_actions, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 def get_hmdb51_client_dataset(client_id, fold, hmdb51_root):
 
@@ -36,6 +72,10 @@ def get_hmdb51_client_dataset(client_id, fold, hmdb51_root):
 
     train_dataset = build_dataset(data_train_cfg)
 
+if __name__ == '__main__':
+
+    preprocessed_dir = sys.argv[1]
+    hmdb51_partition(n_clients=4, preprocessed_dir=preprocessed_dir, fold=1)
 
 
     

47b68b5f9f222a1eb03acd459157f0399bd9c586
quynhkhanh96 <oddball9796@gmail.com>
2022-05-02 11:21:42 +0700

    add build model, loss, optimizer, finish new video client

---
+++ models/build.py
@@ -0,0 +1,103 @@
+import os 
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+
+import resnet3d
+import c3d
+from movinets import MoViNet
+from movinets.config import _C
+from efficientnet_pytorch_3d import EfficientNet3D
+import mobilenet3d_v2
+from slow_fast_r2plus1d import slow_fast_r3d_18
+import init_model
+
+def build_model(cfgs, mode='train'):
+    if cfgs.arch == 'resnet503d':
+        model = resnet3d.resnet50(num_classes=cfgs.num_classes, 
+                                sample_width=cfgs.width, sample_height=cfgs.height, 
+                                sample_duration=cfgs.seq_len)
+        if mode == 'train':
+            if not os.path.exists(cfgs.pretrained_model):
+                raise IOError("Can't find pretrained model: {}".format(cfgs.pretrained_model))
+            print("Loading checkpoint from '{}'".format(cfgs.pretrained_model))
+            checkpoint = torch.load(cfgs.pretrained_model)
+            state_dict = {}
+            for key in checkpoint['state_dict']:
+                if 'fc' in key: continue
+                state_dict[key.partition("module.")[2]] = checkpoint['state_dict'][key]
+            model.load_state_dict(state_dict, strict=False)
+    elif cfgs.arch =='resnet183d':
+        model = resnet3d.resnet18(num_classes=cfgs.num_classes, 
+                                sample_width=cfgs.width, sample_height=cfgs.height, 
+                                sample_duration=cfgs.seq_len)		
+        if mode == 'train':
+            if not os.path.exists(cfgs.pretrained_model):
+                raise IOError("Can't find pretrained model: {}".format(cfgs.pretrained_model))
+            print("Loading checkpoint from '{}'".format(cfgs.pretrained_model))
+            checkpoint = torch.load(cfgs.pretrained_model)
+            state_dict = {}
+            for key in checkpoint['state_dict']:
+                if 'fc' in key: continue
+                state_dict[key.partition("module.")[2]] = checkpoint['state_dict'][key]
+            model.load_state_dict(state_dict, strict=False)	
+
+    elif cfgs.arch == 'c3d_bn':
+        model = c3d.c3d_bn(num_classes=cfgs.num_classes, dropout=0.2)
+        if mode == 'train':
+            if not os.path.exists(cfgs.pretrained_model):
+                raise IOError("Can't find pretrained model: {}".format(cfgs.pretrained_model))
+            print("Loading checkpoint from '{}'".format(cfgs.pretrained_model))
+            checkpoint = torch.load(cfgs.pretrained_model)
+            model.load_state_dict(checkpoint)	
+
+    elif cfgs.arch == 'movinet_a0':
+        model = MoViNet(_C.MODEL.MoViNetA0, causal=False, pretrained=True)
+        model.classifier[3] = torch.nn.Conv3d(2048, cfgs.num_classes, (1, 1, 1))
+    
+    elif cfgs.arch == 'movinet_a2':
+        model = MoViNet(_C.MODEL.MoViNetA2, causal=False, pretrained = True)
+        model.classifier[3] = torch.nn.Conv3d(2048, cfgs.num_classes, (1, 1, 1))
+    
+    elif cfgs.arch == 'movinet_a5':
+        model = MoViNet(_C.MODEL.MoViNetA5, causal=False, pretrained=True)
+        model.classifier[3] = torch.nn.Conv3d(2048, cfgs.num_classes, (1, 1, 1))
+    
+    elif cfgs.arch == 'efficientnet3d':
+        model = EfficientNet3D.from_name("efficientnet-b0", 
+                                override_params={'num_classes': cfgs.num_classes}, 
+                                in_channels=3)
+    
+    elif cfgs.arch[:14] == 'mobilenet3d_v2':
+        model = mobilenet3d_v2.mobilenet3d_v2(num_classes=cfgs.num_classes,width_mult=cfgs.width_mult)
+        if not os.path.exists(cfgs.pretrained_model):
+            raise IOError("Can't find pretrained model: {}".format(cfgs.pretrained_model))
+        print("Loading checkpoint from '{}'".format(cfgs.pretrained_model))
+        checkpoint = torch.load(cfgs.pretrained_model)
+        model.load_state_dict(checkpoint,strict=False)
+    
+    elif cfgs.arch == 'slow_fast_r3d_18':
+        model = slow_fast_r3d_18(num_classes=cfgs.num_classes,
+                                pretrained=False, progress=True,
+                                alpha=4, beta=8) # 64//beta -->8	
+    else:
+        model = init_model(name=cfgs.arch, num_classes=cfgs.num_classes, loss={'xent', 'htri'})
+    
+    return model 
+
+def build_loss(cfgs):
+    if cfgs.loss == 'CrossEntropyLoss':
+        loss = nn.CrossEntropyLoss()
+    else:
+        raise ValueError(f'No implementation for {cfgs.loss} loss.')
+
+    return loss 
+
+def build_optimizer(cfgs, model):
+    if cfgs.optimizer == 'SGD':
+        optimizer = optim.SGD(model.parameters(), lr=cfgs.lr, momentum=0.9)
+    else:
+        raise ValueError(f'No implementation for {cfgs.optimizer} optimizer.')
+
+    return optimizer
+
--- video_client.py
+++ video_client.py
@@ -2,9 +2,9 @@ from federated_learning import FedAvgVideoClient
 from federated_learning.client.update.video_base import VideoLocalUpdate 
 from evaluation.video_recognition import evaluate_topk_accuracy
 import flwr
-import torch 
 import argparse
 from datasets.video_dataset import get_client_loaders
+from models.build import build_model, build_loss, build_optimizer
 import yaml 
 from utils.parsing import Dict2Class
 
@@ -30,77 +30,56 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         "--cfg_path",
-        default='configs/hmdb51_rgb_k400_pretrained.py',
+        default='configs/afosr_movinetA0.yaml',
         type=str,
         help="Configuration file path",
-    )
-    parser.add_argument(
-        "--fed_cfg_path",
-        type=str,
-        help="Configuration file path",
-    )
-    parser.add_argument(
-        "--work_dir",
-        type=str,
-        help="where checkpoints are saved, progress is logged, etc",
     )
     parser.add_argument(
         "--data_dir",
         type=str,
-        default='', # should be ${MMACTION}/data/hmdb51
         help="image, metadata directory",
     )
     parser.add_argument(
         "--fold", type=int, default=1, help="split id"
     )
-
-    # datasets
-
-
-    # model
-
-    # local trainer
-
-    # evaluate function
-
-    # start client
-
-
     client_args = parser.parse_args()
     client_id = client_args.cid
 
-    # Configurations 
-    cfg = Config.fromfile(client_args.cfg_path)
-    cfg.omnisource = False # dirty fix 
-    cfg.work_dir = client_args.work_dir + f'/client_{client_id}'
-    cfg.gpu_ids = range(1) #TODO
-
-    with open(client_args.fed_cfg_path, 'r') as yamlfile:
-        fed_cfgs = yaml.load(yamlfile, Loader=yaml.FullLoader)
-    fed_cfgs = Dict2Class(fed_cfgs)
-
-    # Logger
-    flwr.common.logger.configure(f"client_{client_id}", 
-                                    host=client_args.log_host)
+    # configurations 
+    with open(client_args.cfg_path, 'r') as yamlfile:
+        cfgs = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    fed_cfgs = Dict2Class(cfgs)
 
     # datasets
-    train_dataset, test_dataset = get_client_dataset(client_id, client_args.fold,
-                                                        client_args.data_dir, cfg)
+    train_loader, test_loader = get_client_loaders(client_id, 
+                                        client_args.data_dir,
+                                        cfgs)
 
-    # Model 
-    model = build_model( 
-        cfg.model,
-        train_cfg=cfg.get('train_cfg'),
-        test_cfg=cfg.get('test_cfg'))
+    # model
+    model = build_model(cfgs, mode='train')
 
-    # local trainer and evaluation function
-    local_update = MMAction2LocalUpdate(train_dataset, cfg)
-    eval_fn = evaluate_video_recognizer
+    # loss 
+    criterion = build_loss(cfgs)
+    # optimizer
+    optimizer = build_optimizer(cfgs, model)
 
-    # Start client 
-    fedavg_client = FedAvgVideoClient(client_id=client_id,
-                        ds_train=train_dataset, ds_test=test_dataset,
-                        model=model, local_update=local_update,
-                        eval_fn=eval_fn, cfg=cfg
-                    )
-    flwr.client.start_client(client_args.server_address, fedavg_client)
+    # local trainer
+    local_update = VideoLocalUpdate(train_loader=train_loader,
+                                    loss_fn=criterion,
+                                    optimizer=optimizer)
+
+    # evaluate function
+    eval_fn = evaluate_topk_accuracy
+
+    # start client
+    if cfgs.FL == 'FedAvg':
+        fl_client = FedAvgVideoClient(client_id=client_id,
+                dl_train=train_loader, dl_test=test_loader,
+                model=model, loss_fn=criterion, 
+                optimizer=optimizer, local_update=local_update, 
+                eval_fn=eval_fn, cfgs=cfgs
+        )
+    else:
+        raise ValueError(f'No implementation for {cfgs.FL}.')
+
+    flwr.client.start_client(client_args.server_address, fl_client)

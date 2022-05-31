4c23e30e3b41bc656ce0d39f724c2422bbc562cb
quynhkhanh96 <oddball9796@gmail.com>
2022-05-31 11:00:38 +0700

    add img resize in build_rawframes

--- tools/build_rawframes.py
+++ tools/build_rawframes.py
@@ -3,13 +3,35 @@ import sys
 import mmcv 
 import warnings
 
-def extract_frames(video_path, out_path):
+def extract_frames(video_path, out_path, new_height, new_width):
 
     try:
         vr = mmcv.VideoReader(video_path)
         for i, vr_frame in enumerate(vr):
             if vr_frame is not None:
-                mmcv.imwrite(vr_frame, f'{out_path}/img_{i}.jpg')
+                # w, h, _ = np.shape(vr_frame)
+                # if args.new_short == 0:
+                #     if new_width == 0 or new_height == 0:
+                #         # Keep original shape
+                #         out_img = vr_frame
+                #     else:
+                #         out_img = mmcv.imresize(
+                #             vr_frame,
+                #             (new_width, new_height))
+                # else:
+                #     if min(h, w) == h:
+                #         new_h = args.new_short
+                #         new_w = int((new_h / h) * w)
+                #     else:
+                #         new_w = args.new_short
+                #         new_h = int((new_w / w) * h)
+                #     out_img = mmcv.imresize(vr_frame, (new_h, new_w))
+                if new_width == 0 or new_height == 0:
+                    # Keep original shape
+                    out_img = vr_frame 
+                else:
+                    out_img = mmcv.imresize(vr_frame, (new_width, new_height))
+                mmcv.imwrite(out_img, f'{out_path}/img_{i}.jpg')
             else:
                 warnings.warn(
                     'Length inconsistent!'
@@ -22,9 +44,15 @@ def extract_frames(video_path, out_path):
 
 src_dir = sys.argv[1]
 dst_dir = sys.argv[2]
+try:
+    new_height, new_width = sys.argv[3], sys.argv[3]
+except:
+    new_height, new_width = 0, 0
 os.makedirs(dst_dir, exist_ok=True)
 
-for person_id in os.listdir(src_dir):
+person_ids = os.listdir(src_dir)
+print('Total person ids =', len(person_ids))
+for iter_, person_id in enumerate(person_ids):
     os.makedirs(dst_dir + '/' + person_id, exist_ok=True)
     for timestamp in os.listdir(src_dir + '/' + person_id):
         os.makedirs(dst_dir + '/' + person_id + '/' + timestamp, exist_ok=True)
@@ -41,4 +69,4 @@ for person_id in os.listdir(src_dir):
 
             extract_frames(video_path, out_path)
 
-    print('DONE 01 PERSON.')
+    print(f'DONE {iter_ + 1} PEOPLE.')
